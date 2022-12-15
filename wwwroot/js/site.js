583d28de1708c98feeff49b557a09368702d1b66
aDOTyang <alexyang93@gmail.com>
2022-12-15 00:01:58 -0500

    updating views to incorporate template; enabled dark mode theme save to local storage for consistency

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var darkMode = document.getElementById("darkMode");
+// check local storage for current value
+darkOn = localStorage.getItem("dark-only") == "true" ? true : false;
+setTheme();
 
-// Write your JavaScript code.
+function setTheme() {
+    //save new value to local storage
+    localStorage.setItem("dark-only", darkOn ? "true" : "false");
+    if (darkOn) {
+        document.body.classList.add('dark-only');
+        //document.body.classList.remove('fa-moon-o');
+        //document.body.classList.add('fa-lightbulb-o');
+    }
+    else {
+        document.body.classList.remove('dark-only');
+        //document.body.classList.remove('fa-lightbulb-o');
+        //document.body.classList.add('fa-moon-o');
+    }
+}
+
+
+function toggle() {
+    darkOn = !darkOn;
+    setTheme();
+}
+
+darkMode.addEventListener("click", toggle);
